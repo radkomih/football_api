e8f1ef9ca3991e917261aceadd27b6f38f6214d7
Radosvet Mihtarski <radkomih@gmail.com>
2020-09-05 21:32:59 +0300

    Add additional odds query methods

--- lib/football_api/odds/odd.rb
+++ lib/football_api/odds/odd.rb
@@ -12,6 +12,30 @@ module FootballApi
           fetch("date/#{date}", options)
         end
 
+        def all_by_date_and_bookmaker(date, bookmaker_id, **options)
+          fetch("date/#{date}/bookmaker/#{bookmaker_id}", options)
+        end
+
+        def all_by_fixture_and_bookmaker(fixture_id, bookmaker_id, **options)
+          fetch("fixture/#{fixture_id}/bookmaker/#{bookmaker_id}", options)
+        end
+
+        def all_by_fixture_and_label(fixture_id, label_id, **options)
+          fetch("fixture/#{fixture_id}/label/#{label_id}", options)
+        end
+
+        def all_by_league(league_id, **options)
+          fetch("league/#{league_id}", options)
+        end
+
+        def all_by_league_and_bookmaker(league_id, bookmaker_id, **options)
+          fetch("league/#{league_id}/bookmaker/#{bookmaker_id}", options)
+        end
+
+        def all_by_league_and_label(league_id, label_id, **options)
+          fetch("league/#{league_id}/label/#{label_id}", options)
+        end
+
         private
 
         def resource
