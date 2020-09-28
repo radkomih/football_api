6062ec5883297ca4f09b387a6d6d52b577eed243
Rado M <radkomih@gmail.com>
2020-09-28 23:09:37 +0300

    Fix when host ends with slash (#10)

--- lib/football_api/connection.rb
+++ lib/football_api/connection.rb
@@ -9,12 +9,12 @@ module FootballApi
       raise 'Missing API key or Host' unless FootballApi.configuration.api_host ||
                                              FootballApi.configuration.api_key
 
-      url = URI("#{FootballApi.configuration.api_host}/#{endpoint}")
+      url = URI(FootballApi.configuration.api_host.split('/').push(endpoint).join('/'))
       http = Net::HTTP.new(url.host, url.port)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       connection = Net::HTTP::Get.new(url)
-      connection["x-rapidapi-host"] = FootballApi.configuration.api_host
+      connection["x-rapidapi-host"] = url.host
       connection["x-rapidapi-key"] = FootballApi.configuration.api_key
 
       response = http.request(connection)
